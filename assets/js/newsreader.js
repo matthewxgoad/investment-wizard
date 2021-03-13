6a4dfdd24bc0760efbeef192c7962f46eaa4147c
spenserlogan <spenserlogan@gmail.com>
2021-03-13 14:05:47 -0600

    updated mewsreader

--- assets/js/newsreader.js
+++ assets/js/newsreader.js
@@ -1,4 +1,5 @@
 // text input from search box is #search-input
+
 // search button is #search-btn
 
 // right side container has id #right-side
@@ -6,3 +7,21 @@
 // create new div elements with the following classes
 // tile is-child box
 // add them to the div with id #stock-news-items
+
+//global Variables
+var url = 
+
+
+
+//functions
+const searchButton = document.getElementById('search-btn')
+searchButton.addEventListener('click', buttonSubmit)
+//
+function buttonSubmit() {
+    let searchInput = document.getElementById('search-input')
+    let userInput = searchInput.value
+    userInput = userInput.toUpperCase().trim()
+    fetchStockPrice(userInput)
+}
+
+//eventlisteners
