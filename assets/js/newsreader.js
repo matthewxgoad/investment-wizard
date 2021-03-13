22e6cb4971736d0a749e61cf82ec3ce97cc37cd0
Matthew Goad <matthewxgoad@gmail.com>
2021-03-13 16:18:19 -0600

    commented out code in newsreader.js that was duplicated from ticketreader as it was breaking the formatting

--- assets/js/newsreader.js
+++ assets/js/newsreader.js
@@ -9,19 +9,21 @@
 // add them to the div with id #stock-news-items
 
 //global Variables
-var url = 
+// var url = 
 
 
 
 //functions
-const searchButton = document.getElementById('search-btn')
-searchButton.addEventListener('click', buttonSubmit)
+// const searchButton is already definied in ticketreader.js
+// const searchButton = document.getElementById('search-btn')
+// searchButton.addEventListener('click', buttonSubmit)
 //
-function buttonSubmit() {
-    let searchInput = document.getElementById('search-input')
-    let userInput = searchInput.value
-    userInput = userInput.toUpperCase().trim()
-    fetchStockPrice(userInput)
-}
+// function buttonSubmit() is already definied in ticketreader.js
+// function buttonSubmit() {
+//     let searchInput = document.getElementById('search-input')
+//     let userInput = searchInput.value
+//     userInput = userInput.toUpperCase().trim()
+//     fetchStockPrice(userInput)
+// }
 
 //eventlisteners
